416ca3daf351f7f1068e7addf71f365374f131e0
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-06-25 18:53:18 -0400

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :artworks
     has_secure_password
+    validates :username, presence: true
+    validates :password, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,40 @@
 Gurjot = User.create(username:"g",password:"123") 
 
-Animal = Community.create(name:"animal")
+Animal = Community.create(name:"Animals")
 
-cars = Community.create(name:"cars")
+cars = Community.create(name:"Cars")
 
-art= Artwork.create(name:"tiger",image:"https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",user_id:1,community_id:1)
+Boomers = Community.create(name:"Boomers")
 
-art2= Artwork.create(name:"kitties",image:"https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",user_id:1,community_id:1)
+Movies = Community.create(name:"Good Movies")
+
+Movies2 = Community.create(name:"Movies that should never have been made")
+
+Goodboys = Community.create(name:"Goodest boys")
+
+Whisk = Community.create(name:"Whiskers")
+
+inv = Community.create(name:"Invisible")
+
+Memes = Community.create(name:"Memes")
+
+Digital = Community.create(name:"Digital Art")
+
+Portraits = Community.create(name:"Portraits")
+
+Paintings = Community.create(name:"Paintings")
+
+Vid = Community.create(name:"Video Game")
+
+big = Community.create(name:"Cartoons")
 
 
-art3= Artwork.create(name:"aston martin coupe",image:"https://media.ed.edmunds-media.com/aston-martin/vantage/2019/oem/2019_aston-martin_vantage_coupe_base_f_oem_1_815.jpg",user_id:1,community_id:2)
+
+
+
+# art= Artwork.create(name:"tiger",image:"https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",user_id:1,community_id:1)
+
+# art2= Artwork.create(name:"kitties",image:"https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",user_id:1,community_id:1)
+
+
+# art3= Artwork.create(name:"aston martin coupe",image:"https://media.ed.edmunds-media.com/aston-martin/vantage/2019/oem/2019_aston-martin_vantage_coupe_base_f_oem_1_815.jpg",user_id:1,community_id:2)
